66e561817a7d7b0867703800cd5bf6089515ad12
ifloresg <ifloresg.dev@gmail.com>
2025-11-11 15:39:22 -0600

    comentarios e información

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+#   Ileana Flores García
+#   https://github.com/ifloresg
+#   Student number: 25279
+#   
+#   Isis Atenea Arzola Hernández
+#   https://github.com/IsisArzola
+#   Student number: 25420
+#   
+#   DSM04C
+#   Tuesday, November 11th, 2025
+#   Subject: Mobile app development
+#   ACT3_PRJCT: mobile app
+#   Profe. Felipe Tijerina Martínez
+#   Desarrollo de Software Multiplataforma
+#   Universidad Tecnológica Santa Catarina
+#   Santa Catarina, Nuevo León, México
+#   
+#   This will be an app to display cute cat images. Have fun!
+
 import kivymd
 import kivy
